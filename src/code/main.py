bfa597727db0d72f2a9cff3a476fa5d612df977c
kaibas01 <kaibas01@allegheny.edu>
2024-03-19 14:00:17 -0400

    class & functions added

---
+++ src/code/main.py
@@ -0,0 +1,11 @@
+"""This is the main file of the project"""
+
+
+
+class SearchForEverything:
+    """This is the class for calling other files and giving the right output for the user."""
+    
+    def call_files():
+        """Calling other classes from files."""
+        return 0
+    
