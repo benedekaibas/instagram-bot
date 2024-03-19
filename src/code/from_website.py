7411276a798b2eeff3387aaad2b6c9ef0989f2ed
kaibas01 <kaibas01@allegheny.edu>
2024-03-19 15:56:58 -0400

    functions added

--- src/code/from_website.py
+++ src/code/from_website.py
@@ -1,3 +1,9 @@
 
 class SearchFromWebsite:
     """Class for scraping website."""
+    def __init__(self) -> None:
+        """Init function for the class."""
+        pass
+
+    def read_website(self):
+        return 0
