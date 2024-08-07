181e6385bba3924a8298d91ce6ff5844f0f37f0a
benedekaibas <kaibas.benedek02@gmail.com>
2024-08-07 18:24:21 +0200

    bug fix

--- src/code/delete.py
+++ src/code/delete.py
@@ -3,7 +3,25 @@
 def palindrome(word):
     return word[::-1]
 
-
+"""
 if __name__ == "__main__":
     word = "this is a word"
     print(palindrome(word))
+"""
+
+# Question 3: Write a Python program to find the largest element in a list.
+
+def find_element(list):
+    list = []
+
+    for element in list:
+        if element > element:
+            print(element)
+        else:
+            print(list)
+
+
+if __name__ == "__main__":
+    find_element_list = [1,2,3,4,5]
+    find_element_solution = palindrome(find_element_list)
+    print(find_element_solution)
